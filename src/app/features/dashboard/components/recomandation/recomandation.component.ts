4e2116fbdc6e0eb3b8268d7aab0310824bf7ce3c
Mallikarjun <s.mallikarjun@spekond.com>
2025-07-12 00:38:53 +0530

    Update recommendation component to utilize weather data for dynamic suggestions, enhancing user experience with tailored advice based on current weather conditions.

--- src/app/features/dashboard/components/recomandation/recomandation.component.ts
+++ src/app/features/dashboard/components/recomandation/recomandation.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-recomandation',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule],
   templateUrl: './recomandation.component.html',
   styleUrl: './recomandation.component.scss'
 })
 export class RecomandationComponent {
-
+  @Input() weather: any;
 }
